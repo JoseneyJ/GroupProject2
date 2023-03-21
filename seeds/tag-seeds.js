23625a903330f661dc1a47b3ddd8a9bfcd299ffb
Nikola Lucic <nikola1983@yahoo.com>
2023-03-21 19:52:14 -0400

    created category file

---
+++ seeds/tag-seeds.js
@@ -0,0 +1,26 @@
+const { Tag } = require("../models");
+
+const tagData = [
+  {
+    tag_name: "succulents",
+  },
+  {
+    tag_name: "easy maintenance",
+  },
+  {
+    tag_name: "multicolor",
+  },
+  {
+    tag_name: "rare",
+  },
+  {
+    tag_name: "evergreen",
+  },
+  {
+    tag_name: "fruits and berries",
+  },
+];
+
+const seedTags = () => Tag.bulkCreate(tagData);
+
+module.exports = seedTags;
