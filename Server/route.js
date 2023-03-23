228407f9218f57a99fe5bb21600391af09e81142
SilverDiligent <alexisc@alexislab.com>
2023-03-22 22:03:48 -0400

    Created a POST API

--- Server/route.js
+++ Server/route.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+
+//middleware backend sent to frontend
+app.use(bodyParser.json());
 
 //loading environment variables from a .env file and making them
 //available in the process.env object.
 require("dotenv").config();
 const apiDatabaseUrl = process.env.API_URL;
-const apiKey = process.env.api_Key;
 
 // We are going to create an initial API route
-app.get("/", (req, res) => {
-  res.send("Hello World of API !");
+app.get(`${apiDatabaseUrl}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "Thyme",
+    image: "some url",
+  };
+  res.send(product);
 });
+
+app.post(`${apiDatabaseUrl}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
+});
+
 // start the server and listen for incoming requests
 app.listen(3000, () => {
-  console.log(apiDatabaseUrl);
   console.log("server is listening on http://localhost:3000");
 });
